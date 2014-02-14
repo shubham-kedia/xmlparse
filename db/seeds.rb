12673d9d9ef98accae5bf82776b9ed8132cf5168
Shubham <shubham.kedia2010@gmail.com>
2014-02-15 05:19:03 +0530

    1 hour added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = Admins.new(:email => "admin@admin.com",:password => "adminadmin", :password_confirmation => "adminadmin")
