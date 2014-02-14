5f14d90c1f91b14d1d0fed81350e76817309dfe7
Shubham <shubham.kedia2010@gmail.com>
2014-02-14 07:53:54 +0530

    parsing with scheduler

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xmltask::Application.config.secret_key_base = '69c6447021be14cef3d5bd67d8ca1d88c9dfae41278a3e3ce0efffbca9ca41368dd0f3f4ff7402ad4885f62a39521e11d70d541f97d0fee497acad5705c3b70a'
