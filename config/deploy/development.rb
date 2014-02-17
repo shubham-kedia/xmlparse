e3fc14c1b0d45def9949bc3a8f306770fd4f4141
Divya <divya.shravanthi@codingmart.com>
2014-02-17 14:34:42 +0530

    development file updated

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -7,7 +7,7 @@ set :rails_env, "development" #added for delayed job
 set :rvm_type, :system
 
 
-before 'after:update_code' do
+after 'deploy:update_code' do
   # run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
   run "cd #{release_path};"
   run "mkdir -p #{release_path}/tmp/cache;"
