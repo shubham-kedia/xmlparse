af31f58b3721642a38f0157dda935ea75b90172b
Shubham <shubham.kedia2010@gmail.com>
2014-02-25 17:32:52 +0530

    log update errors resolved

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,19 +10,16 @@ class CategoriesController < ApplicationController
 		scheduler = Rufus::Scheduler.new
 		scheduler.every '2h' do
 			doc = Nokogiri::XML(open("http://api-product.skimlinks.com/categories?key=8bf53d38d24f389b6d35ef4014a48dad&format=xml"))
+			# doc = Nokogiri::XML(File.open("/home/sk/Documents/xmltask/category.xml"))
 			@count = 0
 			categories = doc.search("//category")
 			categories.each do |category|
-				if @c=Category.where("category_id = ? and provider_name = ?",category.at('id').content ,"skimlinks").first
-					if @c.update_attribute(:name,category.at('name').text)
-						@count = @count+1
-					end
-				else
-					c = Category.new(:provider_name => "skimlinks",:category_id => category.at('id').content,:name => category.at('name').text)
-					# puts category.at('id').content
-					
-					c.save
+				@c= Category.find_or_initialize_by_category_id_and_provider_name(category.at('id').content,"skimlinks")
+				if @c.name.present? && @c.name !=category.at('name').text
+					@count= @count+1
 				end
+				@c.name = category.at('name').text
+				@c.save
 			end
 			@l=Log.new(:records_updated => @count )
 			@l.save
@@ -34,13 +31,9 @@ class CategoriesController < ApplicationController
 			# puts doc.root
 			categories = doc.search("//category")
 			categories.each do |category|
-				if @c=Category.where("category_id = ? and provider_name = ?",category.at('id').content ,"skimlinks").first
-					@c.update_attribute(:name,category.at('name').text)
-				else
-					c = Category.new(:provider_name => "skimlinks",:category_id => category.at('id').content,:name => category.at('name').text)
-					# puts category.at('id').content
-					c.save
-				end
+				@c= Category.find_or_initialize_by_category_id_and_provider_name(category.at('id').content,"skimlinks")
+				@c.name =category.at('name').text
+				@c.save
 			end
 			render :text => "XML Data Insertion Done"
 	end
